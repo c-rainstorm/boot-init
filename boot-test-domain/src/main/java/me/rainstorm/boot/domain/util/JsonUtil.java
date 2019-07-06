eaa2d1b1fb81191f0461bea8f20c08e14e76b996
Mr.恒尘 <rainstorm.me@outlook.com>
2019-07-07 00:38:53 +0800

    [Update] 删除 JsonUtil 的 @Resource 注解

--- boot-test-domain/src/main/java/me/rainstorm/boot/domain/util/JsonUtil.java
+++ boot-test-domain/src/main/java/me/rainstorm/boot/domain/util/JsonUtil.java
@@ -25,7 +25,6 @@ import java.util.Objects;
 public class JsonUtil {
     private static final String CATEGORY = JsonUtil.class.getSimpleName();
 
-    @Resource
     private static ObjectMapper objectMapper;
 
     static {
